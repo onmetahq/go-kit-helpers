1063234d30218bf94d32a08c464bd1ae1828479e
krishna <krishna@krishnas-Air.Dlink>
2022-12-03 13:49:46 +0530

    adding path and method in logging

--- pkg/utilities/logging.go
+++ pkg/utilities/logging.go
@@ -6,16 +6,17 @@ import (
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/onmetahq/go-kit-helpers/pkg/logger"
+	"github.com/onmetahq/go-kit-helpers/pkg/models"
 )
 
 func Logger(logger logger.CtxLogger) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
 			startTime := time.Now()
-			logger.Context(ctx).Debug().Log("msg", "Server request started", "request", request)
+			logger.Context(ctx).Debug().Log("msg", "Server request started", "request", request, "path", ctx.Value(models.URLPath), "method", ctx.Value(models.HttpMethod))
 			res, err := next(ctx, request)
 			elapsedTime := time.Since(startTime)
-			logger.Context(ctx).Debug().Log("msg", "Server request ended", "response", res, "duration", elapsedTime.Milliseconds())
+			logger.Context(ctx).Debug().Log("msg", "Server request ended", "response", res, "duration", elapsedTime.Milliseconds(), "path", ctx.Value(models.URLPath), "method", ctx.Value(models.HttpMethod))
 			return res, err
 		}
 	}
