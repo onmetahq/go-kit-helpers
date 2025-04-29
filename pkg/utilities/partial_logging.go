9409670499fa82334ada005d08798f5113d17206
krishna <krishnateja262@gmail.com>
2025-04-29 14:39:38 +0530

    adding partial logger which logs complete path only when there is error

---
+++ pkg/utilities/partial_logging.go
@@ -0,0 +1,29 @@
+package utilities
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/log"
+	ctxLogger "github.com/onmetahq/go-kit-helpers/pkg/logger"
+	"github.com/onmetahq/go-kit-helpers/pkg/models"
+)
+
+func PartialLogger(logger log.Logger) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (interface{}, error) {
+			lg := ctxLogger.NewCtxLogger(logger)
+
+			startTime := time.Now()
+			res, err := next(ctx, request)
+			elapsedTime := time.Since(startTime)
+			if err == nil {
+				lg.Context(ctx).Info().Log("msg", "Server request ended", "duration", elapsedTime.Milliseconds(), "path", ctx.Value(models.URLPath), "method", ctx.Value(models.HttpMethod))
+			} else {
+				lg.Context(ctx).Error().Log("msg", "Server request ended with error", "request", request, "response", res, "error", err, "duration", elapsedTime.Milliseconds(), "path", ctx.Value(models.URLPath), "method", ctx.Value(models.HttpMethod))
+			}
+			return res, err
+		}
+	}
+}
