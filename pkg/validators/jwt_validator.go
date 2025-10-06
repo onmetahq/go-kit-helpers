c12ef0fcf060e57680a3863708f7ee557cbdacc6
Adintah <adinathsangale2017@gmail.com>
2025-10-06 19:00:33 +0530

    token blacklist check added

--- pkg/validators/jwt_validator.go
+++ pkg/validators/jwt_validator.go
@@ -1,14 +1,71 @@
 package validators
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/golang-jwt/jwt"
 	"github.com/onmetahq/go-kit-helpers/pkg/models"
 	ctxKeys "github.com/onmetahq/meta-http/pkg/models"
 )
+
+type BlacklistRequest struct {
+	AccessToken string `json:"accessToken"`
+}
+
+type BlacklistResponse struct {
+	Success bool `json:"success"`
+	Data    struct {
+		IsBlacklisted bool `json:"isBlacklisted"`
+	} `json:"data"`
+}
+
+func checkTokenBlacklist(tokenString string) (bool, error) {
+	slog.Debug("Checking token blacklist", "token", tokenString)
+	url := os.Getenv("BLACKLIST_API_URL")
+	if url == "" {
+		return false, fmt.Errorf("BLACKLIST_API_URL environment variable is not set")
+	}
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		return false, fmt.Errorf("API_KEY environment variable is not set")
+	}
+	
+	payload, _ := json.Marshal(BlacklistRequest{AccessToken: tokenString})
+	
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return false, err
+	}
+	
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("apikey", apiKey)
+	
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("API returned status code: %d", resp.StatusCode)
+	}
+	
+	var blacklistResp BlacklistResponse
+	if err := json.NewDecoder(resp.Body).Decode(&blacklistResp); err != nil {
+		return false, err
+	}
+	
+	return blacklistResp.Success && blacklistResp.Data.IsBlacklisted, nil
+}
 
 func JWTValidator(hmacSecret string) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
@@ -48,6 +105,15 @@ func JWTValidator(hmacSecret string) endpoint.Middleware {
 
 			if !token.Valid {
 				slog.ErrorContext(ctx, "Invalid JWT")
+				return nil, models.ErrUnauthorized
+			}
+
+			// Check if token is blacklisted
+			isBlacklisted, err := checkTokenBlacklist(tokenString)
+			if err != nil {
+				slog.ErrorContext(ctx, "Failed to check token blacklist", "error", err)
+			} else if isBlacklisted {
+				slog.ErrorContext(ctx, "Token is blacklisted")
 				return nil, models.ErrUnauthorized
 			}
 
